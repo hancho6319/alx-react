05b6a43f67abc06da398764b0fa5abde38ae7d97
Haruna Auwalu <hancho6319@hancho6319.hancho6319>
2024-06-26 03:14:44 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import holbertonLogo from './holberton-logo.png';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holbertonLogo} className="App-logo" alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <hr/>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+        <br />
+        <button>OK</button>
+      </div>
+      <hr/>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Notifications from './Notifications';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>
+      <div id="root-notifications">
+        <Notifications />
+      </div>
+      <div id="root">
+        <App />
+      </div>
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
