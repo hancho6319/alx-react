ca188d7569167e3d5bf5ae215cc801caf72ac048
Haruna Auwalu <hancho6319@hancho6319.hancho6319>
2024-06-29 05:35:45 +0100

    GitHub repository: alx-react     Directory: 0x00-Webpack     File: task_2/package.json, task_2/css/main.css, task_2/webpack.config.js, task_2/js/dashboard_main.js, task_2/public/index.html

---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/',
+              publicPath: 'assets/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
+
