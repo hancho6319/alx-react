369e36c13334a2c6bca174cd792838e325b96000
Haruna Auwalu <hancho6319@hancho6319.hancho6319>
2024-07-20 03:55:19 +0100

    GitHub repository: alx-frontend-for-fun     Directory: form     File: 01-article.html, 01-styles.css

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,9 +1,19 @@
 // 0-fromjs.js
 const { fromJS } = require('immutable');
 
-function getImmutableObject(object) {
+function getImmutableObject (object) {
   return fromJS(object);
 }
 
 module.exports = getImmutableObject;
 
+// Example usage
+const exampleObject = {
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132
+};
+
+const immutableObject = getImmutableObject(exampleObject);
+console.log(immutableObject);
