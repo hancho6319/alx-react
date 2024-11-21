1a959d8825e5c21248be498db3c8ae17c6323e32
hancho6319 <abbaauwalharuna@gmail.com>
2024-11-21 09:52:06 +0100

    Added notification action types, creators, and tests in task_5

---
+++ 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/actions/notificationActionTypes.js
@@ -0,0 +1,9 @@
+// Action Types
+export const MARK_AS_READ = "MARK_AS_READ";
+export const SET_TYPE_FILTER = "SET_TYPE_FILTER";
+
+// Filter States
+export const NotificationTypeFilters = {
+  DEFAULT: "DEFAULT",
+  URGENT: "URGENT",
+};
