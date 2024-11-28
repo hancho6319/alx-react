17830d59796bc8ad0934448b660acb1ebc23bcb2
hancho6319 <abbaauwalharuna@gmail.com>
2024-11-28 09:53:07 +0100

    my commit

--- task_0/src/schema/notifications.js
+++
@@ -1,8 +0,0 @@
-// Import the JSON data
-import * as notificationsData from '../../notifications.json';
-
-// Function to get all notifications by userId
-export function getAllNotificationsByUser(userId) {
-  return notificationsData.default.filter(notification => notification.author.id === userId);
-}
-
